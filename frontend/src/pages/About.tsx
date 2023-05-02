df0f6c1c790f74bbdb13844c55b7d0544ad6a8ca
AnthonyScott <anthony.scott1005@gmail.com>
2023-05-02 16:25:55 -0400

    Finish about page

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,5 +1,38 @@
-export default function About(props:{ URL:string }){
-    return (
-        <h1>About</h1>
-    )
+import { useState, useEffect } from "react";
+
+
+export default function About(props: { URL: string }) {
+    interface About {
+        name: string,
+        email: string,
+        bio: string
+    }
+
+    // create state to hold about data
+    const [about, setAbout] = useState<About | null>(null);
+
+    // create function to make api call
+    const getAboutData = async () => {
+        const response = await fetch("./about.json");
+        const data = await response.json();
+        setAbout(data);
+    };
+
+ 
+    useEffect(() => {getAboutData()}, []);
+
+    if (!about){
+        return <h1>Loading...</h1>
+    }
+    
+    const loaded = () => (
+        <div>
+            <h2>{about.name}</h2>
+            <h3>{about.email}</h3>
+            <p>{about.bio}</p>
+        </div>
+    );
+
+    
+    return loaded();
 }
