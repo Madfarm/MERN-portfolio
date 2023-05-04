b8b71f7ba2be09faf58d2927f81d56504fd77a15
AnthonyScott <anthony.scott1005@gmail.com>
2023-05-04 16:37:50 -0400

    Style intro card

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
     return (
         <section>
@@ -6,7 +8,9 @@ export default function Home() {
                     <h2 className="mt-4 ml-4 ">Web Developer | Software Engineer </h2>
                     <p className="text-xl mt-4 ml-4 p-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti incidunt dolores sed quia praesentium. Quia, saepe at unde adipisci dolorum qui quae quam! Eum necessitatibus, animi fuga nulla exercitationem itaque?</p>
                 </div>
-                <button className="rounded-full border-2 p-4 max-w-sm mb-4 ml-4 hover:bg-gray-400">Projects</button>
+                <Link to="/projects" className="flex flex-start">
+                <button className="rounded-full border-2 p-4 w-64 mb-4 ml-4 hover:bg-gray-400">Projects</button>
+                </Link>
             </div>
         </section>
 
